9c793ba56e64523b206db5dd8725743b867abb0c
freelinli <245386003@qq.com>
2021-04-26 23:14:10 +0800

    rc.local: Add do_snake script

---
+++ meta-sdk/recipes-fsl/fsl-rc-local/files/do_snake.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+udhcpc -i eth0
+
+echo "start MQTTAsync_publish process"
+/opt/apps/MQTTAsync_publish &
+
+echo "start MQTTAsync_subscribe process"
+/opt/apps/MQTTAsync_subscribe &
+
+echo "start cloud_protocol process"
+/opt/apps/cloud_protocol &
+
+echo "start camera process"
+/opt/apps/camera &
+
+echo "start motor process"
+/opt/apps/motor &
+
+echo "start sensor process"
+/opt/apps/sensor &
+
+echo "start process done"
--- meta-sdk/recipes-fsl/fsl-rc-local/fsl-rc-local.bbappend
+++ meta-sdk/recipes-fsl/fsl-rc-local/fsl-rc-local.bbappend
@@ -1,2 +1,9 @@
 #Provide our own rc.local file
 FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+
+S = "${WORKDIR}"
+SRC_URI += "file://do_snake.sh"
+
+do_install_append () {
+    install -m 755 ${S}/do_snake.sh ${D}/${sysconfdir}/do_snake.sh
+}
