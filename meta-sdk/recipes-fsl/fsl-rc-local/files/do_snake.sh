b2a82da8b85f27c6fd54ee197734ef3171e3443e
freelinli <245386003@qq.com>
2021-06-13 22:43:54 +0800

    rc.local: Use launcher

--- meta-sdk/recipes-fsl/fsl-rc-local/files/do_snake.sh
+++ meta-sdk/recipes-fsl/fsl-rc-local/files/do_snake.sh
@@ -1,26 +1,6 @@
 #!/bin/sh
 
-#udhcpc -i eth0
-
-echo "start network process"
-/opt/apps/network &
-
-# echo "start MQTTAsync_publish process"
-# /opt/apps/MQTTAsync_publish &
-
-# echo "start MQTTAsync_subscribe process"
-# /opt/apps/MQTTAsync_subscribe &
-
-echo "start cloud_protocol process"
-/opt/apps/cloud_protocol &
-
-echo "start camera process"
-/opt/apps/camera &
-
-echo "start motor process"
-/opt/apps/motor &
-
-echo "start sensor process"
-/opt/apps/sensor &
+echo "start process"
+/opt/apps/launcher &
 
 echo "start process done"
